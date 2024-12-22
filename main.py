a15ba6e560e7fc2477761a154fea766c5d76c273
Taleb88 <tdamaree18@gmail.com>
2024-12-21 19:05:25 -0500

    created player_award_shares_and_player_per_game_merged dataframe

--- main.py
+++ main.py
@@ -1,12 +1,84 @@
 import pandas as pd
 
-all_star_selections = pd.read_csv('csv/All-Star Selections.csv')
+advanced_df = pd.read_csv('csv/advanced.csv')
+all_star_selections_df = pd.read_csv('csv/All-Star Selections.csv')
+end_of_season_teams_voting_df = pd.read_csv('csv/End of Season Teams (Voting).csv')
+end_of_season_teams_df = pd.read_csv('csv/End of Season Teams.csv')
+opponent_stats_per_100_poss_df = pd.read_csv('csv/Opponent Stats Per 100 Poss.csv')
+opponent_stats_per_game_df = pd.read_csv('csv/Opponent Stats Per Game.csv')
+opponent_totals_df = pd.read_csv('csv/Opponent Totals.csv')
+per_36_minutes_df = pd.read_csv('csv/Per 36 Minutes.csv')
+per_100_poss_df = pd.read_csv('csv/Per 100 Poss.csv')
+player_award_shares_df = pd.read_csv('csv/Player Award Shares.csv')
+player_career_info_df = pd.read_csv('csv/Player Career Info.csv')
+player_per_game_df = pd.read_csv('csv/Player Per Game.csv')
+player_play_by_play_df = pd.read_csv('csv/Player Play By Play.csv')
+player_season_info_df = pd.read_csv('csv/Player Season Info.csv')
+player_shooting_df = pd.read_csv('csv/Player Shooting.csv')
+player_totals_df = pd.read_csv('csv/Player Totals.csv')
+team_abbrev_df = pd.read_csv('csv/Team Abbrev.csv')
+team_stats_per_100_poss_df = pd.read_csv('csv/Team Stats Per 100 Poss.csv')
+team_stats_per_game_df = pd.read_csv('csv/Team Stats Per Game.csv')
+team_summaries_df = pd.read_csv('csv/Team Summaries.csv')
+team_totals_df = pd.read_csv('csv/Team Totals.csv')
 
-print(all_star_selections)
+#print(player_award_shares)
+print(player_award_shares_df)
+print(player_per_game_df)
 
-def michael_jordan(df):
-    return df[df['player'] == 'Michael Jordan']
+#merging 
+player_award_shares_and_player_per_game_merged_df = pd.merge(player_award_shares_df,
+                                                          player_per_game_df, 
+                                                          on=['player_id'])
 
-all_star_selections = michael_jordan(all_star_selections)
+print(player_award_shares_and_player_per_game_merged_df)
 
-print(all_star_selections)
+player_award_shares_and_player_per_game_merged_df.\
+    to_excel('player_award_shares_and_player_per_game_merged.xlsx', index=False)
+
+
+# defining a player class that corresponds to the 
+#   player_award_shares_and_player_per_game_merged dataframe
+class Player:
+    def __init__(self, name, season, award, pts_per_game):
+        self.name = name
+        self.season = season
+        self.award = award
+        self.pts_per_game = pts_per_game
+
+    def info(self):
+        return f"{self.name} won the {self.season} {self.award} while averaging {self.pts_per_game} points per game."
+    
+value = player_award_shares_and_player_per_game_merged_df.iloc[5]
+
+player_instance = Player(name=value['player_x'], 
+                         season=value['season_x'], 
+                         award=value['award'],
+                         pts_per_game=value['pts_per_game'])
+
+print(player_instance.info())
+
+
+# converting values to string format
+player_award_shares_and_player_per_game_merged_df['winner'] = \
+    player_award_shares_and_player_per_game_merged_df['winner'].astype(str)
+
+# changing 'nan' values to 'True' in Bobby Jones' case 1983 sixth man of the year
+player_award_shares_and_player_per_game_merged_df['winner'] = \
+    player_award_shares_and_player_per_game_merged_df['winner'].replace('nan', 'True')
+
+# award winners highlighted
+def award_winners_highlighted(x):
+    try:
+        if x == 'True':
+            return 'background-color: green'
+    except:
+        print('cannot highlight selected cell(s)')
+
+player_award_shares_and_player_per_game_merged_styled_df = (
+    player_award_shares_and_player_per_game_merged_df.
+    style.
+    applymap(award_winners_highlighted, subset=['winner'])
+)
+
+player_award_shares_and_player_per_game_merged_styled_df.to_excel('player_award_shares_and_player_per_game_merged.xlsx', index=False)
