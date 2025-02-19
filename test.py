6cdd37027f21edbafa27205aeff4af817a586f71
Taleb88 <tdamaree18@gmail.com>
2025-02-19 11:15:43 -0500

    added more try-except statments in test.py

--- test.py
+++ test.py
@@ -1186,22 +1186,25 @@ player_values = list(player_per_game_df['player'].values)
 
 while True:
 
-    player = input("enter player's name: ")
+    try:
+        player = input("enter player's name: ")
 
-    if player == 'exit':
-        break
+        if player == 'exit':
+            break
 
-    if player in player_values:
-        #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 4:28], axis=1)
-        #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 8:10], axis=1)
-        #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 0:1], axis=1)
-        #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 1:2], axis=1)
-        print(f"{player_per_game_df[(player_per_game_df['player'] == player)]}")
-    elif player not in player_values:
-        print('not exist')
-    else:
+        if player in player_values:
+            #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 4:28], axis=1)
+            #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 8:10], axis=1)
+            #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 0:1], axis=1)
+            #player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 1:2], axis=1)
+            print(f"{player_per_game_df[(player_per_game_df['player'] == player)]}")
+        elif player not in player_values:
+            print('not exist')
+        else:
+            continue
+    except ValueError:
+        print('please enter a number only')
         continue
-
 
 # look up player and year they played in look up player in player_per_game_df
 print(f"\nlook up player (first name and last name required) " 
@@ -1211,24 +1214,28 @@ player_values = list(player_per_game_df['player'].values)
 year_values = list(player_per_game_df['year'].values)
 
 while True:
+    try:
 
-    player = input("enter player's name: ")
-    year = int(input("enter year: "))
+        player = input("enter player's name: ")
+        year = int(input("enter year: "))
 
-    if player == 'exit':
-        break
+        if player == 'exit':
+            break
 
-    if (player in player_values) and (year in year_values):
-        # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 4:28], axis=1)
-        # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 8:10], axis=1)
-        # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 0:1], axis=1)
-        # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 1:2], axis=1)
-        print(f"{player_per_game_df[(player_per_game_df['player'] == player) & \
-                                    (player_per_game_df['year'] == year)]}")
-    elif (player not in player_values) or (year in year_values):
-        print('not exist')
-    else:
-        continue
+        if (player in player_values) and (year in year_values):
+            # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 4:28], axis=1)
+            # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 8:10], axis=1)
+            # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 0:1], axis=1)
+            # player_per_game_df = player_per_game_df.drop(player_per_game_df.iloc[:, 1:2], axis=1)
+            print(f"{player_per_game_df[(player_per_game_df['player'] == player) & \
+                                        (player_per_game_df['year'] == year)]}")
+        elif (player not in player_values) or (year in year_values):
+            print('not exist')
+        else:
+            continue
+    except ValueError:
+        print('please enter a number only')
+        continue        
 
 # =========================== #
 
